b0767d0a0d69643b5323375203e7d8083aba70be
Milan Suk <Milansuk@email.cz>
2021-07-31 16:56:11 +0200

    feat: readme + slack channel from env

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # ModraRuzeScraper
+
+## Running (Powershell)
+
+Set the slack API token
+
+```powershell
+$env:SLACK_API_TOKEN = "<token>"
+$env:SLACK_CHANNEL = "<token>"
+```
+
+Run without notifying the slack channel (for testing the scraper only)
+
+```powershell
+make run
+```
+
+Run with the slack notification.
+
+```powershell
+make run-with-slack
+```
--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,7 @@
 
 module Main where
 
-import Data.Text (Text, intercalate)
+import Data.Text (Text, intercalate, pack)
 import Formatter (formatLunches)
 import Scraper (allLunches)
 import Slack (sendSlackMessage)
@@ -13,7 +13,9 @@ import Control.Monad (when)
 main :: IO ()
 main = do
   token <- getEnv "SLACK_API_TOKEN"
+  slackChannel <- pack <$> getEnv "SLACK_CHANNEL"
   args <- getArgs
+
   lunches <- allLunches
   let formattedLunches = formatLunches lunches
   putStrLn "lunches fetched"
@@ -21,5 +23,5 @@ main = do
 
   when ("--notify-slack" `elem` args) $
     do
-      sendSlackMessage token "gay-sex" formattedLunches
+      sendSlackMessage token slackChannel formattedLunches
       putStrLn "slack notified"
